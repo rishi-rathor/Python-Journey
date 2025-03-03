1f15e36e99be5154f479bb2d0df5001f8481a657
rishi-rathor <140340292+rishi-rathor@users.noreply.github.com>
2025-03-03 21:31:37 +0530

    First Python program - Hello World!
    This is my first Python script, printing Hello World!

---
+++ hello.py
@@ -0,0 +1,5 @@
+# 🚀 My First Python Program - "Hello, World!" by Rishi Rathor
+
+print("Hello, World! 🚀")  
+print("My name is Rishi Rathor, and this is my first step into Python programming! 😃")  
+
